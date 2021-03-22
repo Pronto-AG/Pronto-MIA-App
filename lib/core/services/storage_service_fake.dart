293216a5ae1a72662ff438d69ee73500131cc8a9
Yannick Vogt <yannick.vogt@hotmail.com>
2021-03-22 16:11:46 +0100

    Add documentation example

--- lib/core/services/storage_service_fake.dart
+++ lib/core/services/storage_service_fake.dart
@@ -1,11 +1,24 @@
 import 'storage_service.dart';
 
+/// Provides storage interaction with the counter.
 class StorageServiceFake extends StorageService {
+  /// Retrieves current counter.
+  ///
+  /// Returns the value of the current counter as a Future int.
+  /// ```dart
+  /// getCounterValue() == 11
+  /// ```
   @override
   Future<int> getCounterValue() async {
     return 11;
   }
 
+  /// Saves [value] as new counter.
+  ///
+  /// Takes [value] as an input for the new counter and returns a Future void.
+  /// ```dart
+  /// saveCounterValue(5) == void
+  /// ```
   @override
   Future<void> saveCounterValue(int value) async {}
 }
